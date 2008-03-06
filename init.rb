9bb7d369e4ab3cbd181bedda37a42299c9d35b81
Joe Tanner <joe.tanner@leadmediapartners.com>
2008-03-06 15:58:58 -0700

    added mechanize require

--- init.rb
+++ init.rb
@@ -1,3 +1,5 @@
+require 'rubygems'
+require 'mechanize'
 %w[web_sickle assertions helpers/asp_net helpers/table_reader].each do |file|
   require File.join(File.dirname(__FILE__), 'lib', file + '.rb')
 end
