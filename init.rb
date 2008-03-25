d55b957140672531678d4455d4d51e0a7fee8f25
Joe Tanner <joe.tanner@leadmediapartners.com>
2008-03-25 12:02:31 -0600

    added missing require for hash_proxy

--- init.rb
+++ init.rb
@@ -1,6 +1,6 @@
 require 'rubygems'
 require 'mechanize'
-%w[web_sickle assertions helpers/asp_net helpers/table_reader].each do |file|
+%w[web_sickle assertions hash_proxy helpers/asp_net helpers/table_reader].each do |file|
   require File.join(File.dirname(__FILE__), 'lib', file + '.rb')
 end
 WebSickle::Base.send :include, WebSickle::Assertions
