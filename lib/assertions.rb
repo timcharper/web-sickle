511da15a5ded34f82408abd3167cd77a0c5aaed9
Tim Harper <timcharper@gmail.com>
2008-04-17 01:48:04 -0600

    cleaned up error messages

--- lib/assertions.rb
+++ lib/assertions.rb
@@ -5,19 +5,27 @@ module WebSickle
       unless(expected == actual)
         report_error <<-EOF
 Error: Expected 
-  #{actual.inspect}.to be
-  #{expected.inspect}
+  #{expected.inspect}, but got
+  #{actual.inspect}
 #{message}
 EOF
       end
     end
 
     def assert_select(selector, message)
-      report_error("Error: Expected selector #{selector.inspect} to find a page element, but didn't. #{message}") if (@page / selector).blank?
+      assert_select_in(@page, selector, message)
     end
 
     def assert_no_select(selector, message)
-      report_error("Error: Expected selector #{selector.inspect} to not find a page element, but did. #{message}") unless (@page / selector).blank?
+      assert_no_select_in(@page, selector, message)
+    end
+
+    def assert_select_in(content, selector, message)
+      report_error("Error: Expected selector #{selector.inspect} to find a page element, but didn't. #{message}") if (content / selector).blank?
+    end
+
+    def assert_no_select_in(content, selector, message)
+      report_error("Error: Expected selector #{selector.inspect} to not find a page element, but did. #{message}") unless (content / selector).blank?
     end
 
     def assert_contains(left, right, message = nil)
