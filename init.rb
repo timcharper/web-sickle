dd49d591aef94ebee5518755d059e3fb4dfd882c
Tim Harper <timcharper@gmail.com>
2008-04-21 15:53:17 -0600

    we need to set the hpricot buffer size otherwise Asp.net scrapers fail

--- init.rb
+++ init.rb
@@ -1,5 +1,9 @@
 require 'rubygems'
 require 'mechanize'
+
+Hpricot.buffer_size = 524288
+
+
 %w[web_sickle assertions hash_proxy helpers/asp_net helpers/table_reader mechanize_overrides/mechanize_form].each do |file|
   require File.join(File.dirname(__FILE__), 'lib', file + '.rb')
 end
